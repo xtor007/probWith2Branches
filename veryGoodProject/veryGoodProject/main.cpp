c99e842f6fb101cf47c3bc7b6f9b558499745cb2
Anatoliy <tolxpams@gmail.com>
2021-02-20 19:44:27 +0200

    first

---
+++ veryGoodProject/veryGoodProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  veryGoodProject
+//
+//  Created by Anatoliy Khramchenko on 2/20/21.
+//  Copyright © 2021 Anatoliy Khramchenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
